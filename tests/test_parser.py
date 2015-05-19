8f4bd19d6da579486b7c418b68a8ee7154b417e1
Dustin Chang <1022dustin@gmail.com>
2015-05-19 11:51:44 -0700

    continuation on parsing out special chars

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 #Using files truncate.py and fout.txt false_out.txt tout.txt and true_out.txt to figure out all the special characters
 
 f = open('mytest_additions_ridOfSpecCharsTry.nt', 'r')
@@ -15,8 +16,22 @@ for line in f:
 
 	#print `i` #str(line.isalnum())
 	#i+=1
+
+#For special characters use regex [^\x00-\x7F]
+#Ctrl+Command+G to select all highlighted in sublime
+
+#On line 1088 between M and m in Lui-Mme is the special char
+#line 1771 between n and o in Franois
+#line 1782 between n then a in Franaise
+#line 1791 between i and r in Cimetires
+#line 1792 between r and s in Therse and n and o in Franois
+#line 2437 between n and g in ngres
+#line 3271 between F and t in Fte
+
 for a in attr:
 	ab = ("".join(a))
+	ab = ab.replace("Melis", 'Melies')	#Between the i and s there is a special character, looks like a cursor
+	#ab = ab.replace('–', 'n') #Was a special character; had to do manually; Doesn't catch it with replace funct, have to do manually.
 	ab = ab.replace('>', '')
 	ab = ab.replace('"', '')
 	ab = ab.replace('.', '')
@@ -24,7 +39,7 @@ for a in attr:
 	ab = ab.replace('(', '')
 	ab = ab.replace(')', '')
 	ab = ab.replace(';', '')
-	#ab = ab.replace('–', '') #This character is the result of some special char not working
+	#ab = ab.replace('', '') #This character is the result of some special char not working
 	ab = ab.replace('!', '')
 	ab = ab.replace('/', '')
 	ab = ab.replace('\\', '')
