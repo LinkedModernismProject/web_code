6c3e84af1cc27e848fc9c7705ee7b263b90c02df
Brayden Arthur <Brayden@w87-173-142.wireless.uvic.ca>
2015-06-04 16:36:40 -0700

    checking something

--- startbootstrap-creative-1.0.1/q/nl2sparql/nl2sparql/basic.py
+++ startbootstrap-creative-1.0.1/q/nl2sparql/nl2sparql/basic.py
@@ -11,3 +11,19 @@ from quepy.parsing import Lemma, Pos, QuestionTemplate, Token, Particle, \
 
 # Openings
 LISTOPEN = Lemma("list") | Lemma("name")
+
+class Thing(Particle):
+    regex = Question(Pos("JJ")) + (Pos("NN") | Pos("NNP") | Pos("NNS")) |\
+            Pos("VBN")
+
+    def interpret(self, match):
+        return HasKeyword(match.words.tokens)
+
+class RelatedTo(QuestionTemplate):
+    """
+    Ex. Person related to thing
+        Thing related to Place
+    """
+
+
+    regex = Lemma("show")
