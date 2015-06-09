e55d36ca5e7733f4fcf41d2e3601a97a57d60dd6
Brayden Arthur <brayden@w134-87-190-228.wireless.uvic.ca>
2015-06-09 10:26:56 -0700

    updating server

--- startbootstrap-creative-1.0.1/q/nl2sparql/nl2sparql/basic.py
+++ startbootstrap-creative-1.0.1/q/nl2sparql/nl2sparql/basic.py
@@ -28,3 +28,25 @@ class RelatedTo(QuestionTemplate):
     """
 
     regex = Lemma("show")
+
+class allOF(QuestionTemplate):
+    """
+    Ex. Show me all ____
+        Are there any ____
+    """
+    regex = Question((Pos("PDT") | Pos("DT") | Pos("PRP$") + (Pos("NN") | Pos("NNP") | Pos("NNS")))
+
+    def interpret(self, match):
+        return self
+
+class LivedIn(QuestionTemplate):
+    """
+    Ex. What ____ ____ lived in ____
+        Are there any ____ from ____
+        Are there any ____ that lived in ____
+    """
+
+
+    def interpret(self, match):
+
+class 
