e6a5f717ebe5c7c17c5a1a5117aeb929751e1f6a
Brayden Arthur <brayden@w134-87-190-228.wireless.uvic.ca>
2015-06-11 15:42:55 -0700

    index stuff

--- startbootstrap-creative-1.0.1/js/d3sparql.js
+++ startbootstrap-creative-1.0.1/js/d3sparql.js
@@ -9,7 +9,8 @@
 var d3sparql = {
   version: "d3sparql.js version 2015-05-25",
   debug: false,  // set to true for showing debug information
-  debug2: true //false for JSON; true for FlareJSON
+  debug2: true, //false for JSON; true for FlareJSON
+  query1234: true
 }
 
 /*
@@ -51,13 +52,13 @@ var d3sparql = {
     </html>
 */
 d3sparql.query = function(endpoint, sparql, callback) {
-  var url = endpoint + "?query=" + encodeURIComponent(sparql) + '&format=text%2Fhtml&timeout=30000&debug=on'
+  var url = endpoint + "?query=" + encodeURIComponent(sparql) + '&timeout=30000&debug=on'
   if (d3sparql.debug) { console.log(endpoint) }
   if (d3sparql.debug) { console.log(url) }
   var mime = "application/sparql-results+json"
   d3.xhr(url, mime, function(request) {
     var json = request.responseText
-    if (d3sparql.debug) { console.log(json) }
+    if (d3sparql.query1234) { console.log(json) }
     json = convert(json) //Converting json to flare.json in convertJSONtoFlare.js
     callback(JSON.parse(json))
   })
