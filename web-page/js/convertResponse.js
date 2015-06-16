0f1dd3db1201d5a72a9bf8dc600cfdc0adc604af
Dustin Chang <1022dustin@gmail.com>
2015-06-16 14:46:51 -0700

    Removed Links still excess code to rm

--- web-page/js/convertResponse.js
+++ web-page/js/convertResponse.js
@@ -2,10 +2,7 @@ function convertResponse (response) {
 	console.log("in convertResponse.js " + typeof(response));
 	var resp = response;
 	console.log(resp);
-	console.log(resp[0] +' '+ resp[3] +' '+ resp[17] +' '+ resp[36]);
-	console.log(resp[29]+'|'+resp[30]+'|'+resp[31]+'|'+resp[32]+'|'+resp[33] +'|'+ resp[34] +'|'+ resp[35] +'|'+ resp[36] +'|'+ resp[37]+'|');	//33 is a newline 34&35 are spaces
-	console.log("Resp: "+ typeof(resp));
-	arrOfLines = resp.match(/.+?\n/g);
+	arrOfLines = resp.match(/.+?\n/g);	//Matches everything until the last newline
 	console.log(arrOfLines);
 	console.log(arrOfLines[0]);
 	console.log(arrOfLines[1]);
@@ -13,7 +10,29 @@ function convertResponse (response) {
 	console.log(arrOfLines[arrOfLines.length-2]);
 	console.log(arrOfLines[arrOfLines.length-1]);
 	console.log(arrOfLines[arrOfLines.length]);
-
+	for(x in arrOfLines) {
+		console.log(x+':'+arrOfLines[x]);
+		//var s = arrOfLines[x].match(/\<td\>[.*?]\>/g);
+		var line_arr = arrOfLines[x].split(">htt");
+		if(line_arr[1]) {
+			var line = line_arr[1].split("/");
+			line = line[line.length-3];
+			line = line.slice(0,-1);
+			if(line.indexOf("#") != -1) {
+				var hash = line.split("#");
+				console.log(hash);
+				line = hash[1];
+			}
+			arrOfLines[x] = line_arr[0]+'>'+line+'</a></td>\n';	//May need <br> or \n at the end
+		}
+	}
+	//Do for loop to get the data into a string
+	var s = '';
+	for (var i = 0; i < arrOfLines.length; i++) {
+		s = s.concat(arrOfLines[i]);
+	};
+	s = s.concat('</table>');
+	console.log(s);
 
 	console.log("going back to Index.html")
 	return resp;
