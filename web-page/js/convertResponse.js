4b525457aa84852f8d48d7b1ad5507a4b4776939
Dustin Chang <1022dustin@gmail.com>
2015-06-16 14:59:31 -0700

    Removed excess

--- web-page/js/convertResponse.js
+++ web-page/js/convertResponse.js
@@ -1,39 +1,23 @@
-function convertResponse (response) {
-	console.log("in convertResponse.js " + typeof(response));
+function convertResponse(response) {
 	var resp = response;
-	console.log(resp);
 	arrOfLines = resp.match(/.+?\n/g);	//Matches everything until the last newline
-	console.log(arrOfLines);
-	console.log(arrOfLines[0]);
-	console.log(arrOfLines[1]);
-	console.log(arrOfLines[2]);
-	console.log(arrOfLines[arrOfLines.length-2]);
-	console.log(arrOfLines[arrOfLines.length-1]);
-	console.log(arrOfLines[arrOfLines.length]);
 	for(x in arrOfLines) {
-		console.log(x+':'+arrOfLines[x]);
-		//var s = arrOfLines[x].match(/\<td\>[.*?]\>/g);
 		var line_arr = arrOfLines[x].split(">htt");
-		if(line_arr[1]) {
+		if(line_arr[1]) {	//Grabs value that is to be presented
 			var line = line_arr[1].split("/");
 			line = line[line.length-3];
 			line = line.slice(0,-1);
-			if(line.indexOf("#") != -1) {
+			if(line.indexOf("#") != -1) {	//If there is a hash with 
 				var hash = line.split("#");
-				console.log(hash);
 				line = hash[1];
 			}
-			arrOfLines[x] = line_arr[0]+'>'+line+'</a></td>\n';	//May need <br> or \n at the end
+			arrOfLines[x] = line_arr[0]+'>'+line+'</a></td>\n';
 		}
 	}
-	//Do for loop to get the data into a string
 	var s = '';
 	for (var i = 0; i < arrOfLines.length; i++) {
 		s = s.concat(arrOfLines[i]);
 	};
-	s = s.concat('</table>');
-	console.log(s);
-
-	console.log("going back to Index.html")
-	return resp;
+	s = s.concat('</table>');	
+	return s;
 }
