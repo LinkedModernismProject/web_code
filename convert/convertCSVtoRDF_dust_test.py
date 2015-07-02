b62e2b3ff5d202013b3aa8e7f053e1d6c86b5e32
Brayden Arthur <brayden@w134-87-190-228.wireless.uvic.ca>
2015-07-02 16:41:29 -0700

    BDP

--- convert/convertCSVtoRDF_dust_test.py
+++ convert/convertCSVtoRDF_dust_test.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python2.7
 '''
-  Converting CSV files to JSON 
-
-2015 Feburary 23
-
-----------------------------------------------------
-By Ashlin Richardson (richardson.ashlin@gmail.com) 
-Developer, Linked Modernisms
-University of Victoria 
-----------------------------------------------------
 
 - In the folder surveyMonkey/Data_All_150205/CSV there are seven .csv files:
   Sheet_1.csv-- Sheet_7.csv
 
 - Re-saved csv files from EXCEL format (xls) data.
-- Replaced (in excel) any commas with **********. 
-- The ********** delimiter is replaced with ',' again, after reading. 
+- Replaced (in excel) any commas with **********.
+- The ********** delimiter is replaced with ',' again, after reading.
 - Input text encoding is tested (it is ISO-8859-1).
-- Data converted to ('utf8' encoding) when output in JSON format. 
+- Data converted to ('utf8' encoding) when output in JSON format.
 - First two lines of CSV file needed to be mashed (to create field names).
-- Special fields (col >=9) with names not {Other*, Response*, and Open*} 
-   were required to contain nothing, or be equal to the name of the field. 
-- All records passed the above test. 
+- Special fields (col >=9) with names not {Other*, Response*, and Open*}
+   were required to contain nothing, or be equal to the name of the field.
+- All records passed the above test.
 
 color Routines:
 '''
@@ -63,7 +54,7 @@ def RED():
         return(str(""));
     else:
         return('\033[91m');
-def DEFAULT(): #return to normal 
+def DEFAULT(): #return to normal
     if(os.name!='posix'):
         return str("");
     else:
@@ -110,7 +101,7 @@ fieldNameForCol = { } ; #142.104.52.1
 responseNames = [ ];
 nCol = 0;
 nCol0 = 0;
-j=0; 
+j=0;
 for row in reader:
   myRow = copy.deepcopy(row);#row[0].split(",");
   myRow = myRow[0].split(',');
@@ -119,23 +110,23 @@ for row in reader:
   if( j==1):
     responseNames = copy.deepcopy(myRow);
   elif( j==0):
-    #fieldNames = copy.deepcopy(myRow);  
+    #fieldNames = copy.deepcopy(myRow);
     #origFieldNames = copy.deepcopy(myRow);
     #nCol =nCol0= len(fieldNames);
     #print(RED()+"Column header("+str(len(fieldNames))+"):"+str(fieldNames));
-    #elif(j==0): 
+    #elif(j==0):
     #    responseNames = copy.deepcopy(myRow);
     fieldNames = copy.deepcopy(myRow);
   else:
     myRows[j]=myRow;
-  j+=1;  
+  j+=1;
 
 #print(BLUE()+str(fieldNames))
 #print(GREEN()+str(responseNames));
 
 #Modified by Dustin here
 mod_uvic = '<http://modernism.uvic.ca/metadata#'  #Just cleaning up for make print line easier to read
-myNames = { } 
+myNames = { }
 for i in myRows:
   row = myRows[i];
   myName = row[0];
@@ -173,7 +164,7 @@ for i in myRows:
       myNames[myName]=1;
 
 #========================================================================
-nameList = [ ] 
+nameList = [ ]
 for name in myNames:
   nameList.append(name);
 
@@ -196,7 +187,7 @@ myData = [ ];
 '''
 Note 1:  Started with code by BeanBagKing at http://stackoverflow.com/questions/19697846/python-csv-to-json
 
-Note 2: Had to apply this technique to remove "null" characters... 
+Note 2: Had to apply this technique to remove "null" characters...
   http://stackoverflow.com/questions/4166070/python-csv-error-line-contains-null-byte
 
 otherwise we would get an error like:
@@ -214,7 +205,7 @@ for i in range(0,7):
   myDataI[i] = { };
   fPfx      = "Sheet_"+str(i+1)+".";
   myFileNames.append( fPfx);
-  csvFile   = fPfx+"csv"; 
+  csvFile   = fPfx+"csv";
   csvFile   = fixNulls(csvFile);
   jsnFile   = fPfx+"json";
 
@@ -222,18 +213,18 @@ for i in range(0,7):
   reader = csv.reader( csvfile, dialect=csv.excel_tab);# fieldnames)
   myRows = { };
   fieldNames = [ ]; origFieldNames[i] = { } ; origResponseNames[i] = { } ;
-  fieldNameForCol = { } ; 
+  fieldNameForCol = { } ;
   responseNames = [ ];
   nCol = 0;
   nCol0 = 0;
-  j=0; 
+  j=0;
   for row in reader:
     myRow = copy.deepcopy(row);#row[0].split(",");
     myRow = myRow[0].split(',');
     for kk in range(0,len(myRow)):
       myRow[kk] = (myRow[kk]).replace('**********',',');
     if( j==0):
-      fieldNames = copy.deepcopy(myRow);  
+      fieldNames = copy.deepcopy(myRow);
       origFieldNames[i] = copy.deepcopy(myRow);
       nCol =nCol0= len(fieldNames);
       print(RED()+"Column header("+str(len(fieldNames))+"):"+str(fieldNames));
@@ -241,12 +232,12 @@ for i in range(0,7):
       responseNames = copy.deepcopy(myRow);
       origResponseNames[i] = copy.deepcopy(myRow);
       print(MAGENTA()+"Column header("+str(len(responseNames))+"):"+str(responseNames));#fieldNames));
-      nCol = len(responseNames);#max(nCol,len(responseNames));  
+      nCol = len(responseNames);#max(nCol,len(responseNames));
       if( nCol > nCol0):
         print("Error: new header length was longer than old header length.");
         import sys;
         sys.exit(1);
-      for k in range(9,len(responseNames)):#nCol0): 
+      for k in range(9,len(responseNames)):#nCol0):
         #print(str(i))
         fieldNames[k] = responseNames[k];
     else:
@@ -256,17 +247,17 @@ for i in range(0,7):
   nRows = j;#copy.deepcopy(j);
   j = 0;
   myFileNRows[ fPfx ] = nRows;
-  myFieldNames = [ ] 
+  myFieldNames = [ ]
   for fieldName in fieldNames:
     if(fieldName.strip()!=''):
       if( j>8):
         print (RED() + str(j)+",")+(GREEN() + str(fPfx)) + (BLUE()+str(fieldName))
       myFieldNames.append(fieldName);
-      fieldNameForCol[ j ] = fieldName; 
+      fieldNameForCol[ j ] = fieldName;
     j+=1;
-  j=0; 
+  j=0;
   for j in range(2,nRows):
-    row = myRows[j]; 
+    row = myRows[j];
     dataElement = { };
     print(GREEN()+str(row));
     errorThisRow = False;
@@ -331,23 +322,23 @@ for d in jsonData:
 
 
 
-joinData = { } 
+joinData = { }
 for j in range(2,nRows):
   joinData[j] = { }; #add a dictionary element for this row.
   for i in range(0,7):
     try:
-      d0 = (myDataI[i])[j] #print x0    
+      d0 = (myDataI[i])[j] #print x0
       di = -1;
       for d in d0:
         di+=1;
         if d in joinData[j]:
           #print(GREEN()+"Warning: key was already present in record");
           if(str(d0[d])!=str((joinData[j])[d])):
-            print(RED()+"Warning-- duplicate keys had non-matching values-- key="+str(d)+"-->"+str(d0[d]) +","+ str((joinData[j])[d])) 
+            print(RED()+"Warning-- duplicate keys had non-matching values-- key="+str(d)+"-->"+str(d0[d]) +","+ str((joinData[j])[d]))
             print(GREEN()+"-----"+str(  (  ( origFieldNames[i]).split(","))[di])+","+str(  ( (origResponseNames[i]).split(","))[di]))
             #import sys; sys.exit(1);
         (joinData[j])[d] = d0[d];
-    except: 
+    except:
       print(RED()+"Error: i,j=("+str(i)+","+str(j));
       #import sys; sys.exit(1);
 print(MAGENTA()+"2--"+str(nRows)+" rows processed for all 7 files"+GREEN())
@@ -356,33 +347,33 @@ print(MAGENTA()+"2--"+str(nRows)+" rows processed for all 7 files"+GREEN()+"-- t
 #for j in range(2,nRows):
 #  print joinData[j]
 
-for i in range(0,7): 
+for i in range(0,7):
   '''print out first two rows of csv'''
   print RED()+str(origFieldNames[i])
   print GREEN()+str(origResponseNames[i])
 
 '''---- do not use ID (row index) as identifier. Use the ANSWER to the long-format question string.. (check for consistency)... '''
-'''---- delete A through I ----- do not expose... ''' 
-''' -- do not need question at all...?? ---''' 
+'''---- delete A through I ----- do not expose... '''
+''' -- do not need question at all...?? ---'''
 
 
 
 '''
- (1)   "Columns should be relabeled corresponding to lexicon of ontology.. JSON should be instance of ontology..." 
+ (1)   "Columns should be relabeled corresponding to lexicon of ontology.. JSON should be instance of ontology..."
    instead of "design"... say, (subject, object, predicate)..."is a", "design", "T/F"
 
-   SPARQL... show me all the subjects for which  predicate "is a" corresponds to object.. 
+   SPARQL... show me all the subjects for which  predicate "is a" corresponds to object..
 '''
 
-''' 
-  (2) for binary questions, do you want true/false rather than yes/no.... answer this from teh ontology.. GroupOrMovement,True/False... (translate Yes/No into True/False).. 
+'''
+  (2) for binary questions, do you want true/false rather than yes/no.... answer this from teh ontology.. GroupOrMovement,True/False... (translate Yes/No into True/False)..
 
-  ( ) need to make a translation table between the col. names that are there, and the ontology names...  (critical/controlled vocabulary).... I.E., need to translate the QUESTIONS into vocabulary (object-predicate relationship) from the Ontology... 
+  ( ) need to make a translation table between the col. names that are there, and the ontology names...  (critical/controlled vocabulary).... I.E., need to translate the QUESTIONS into vocabulary (object-predicate relationship) from the Ontology...
 
-isa,object .... isa, installation.... associatedWith,dance.. associatedwith,magic... 
+isa,object .... isa, installation.... associatedWith,dance.. associatedwith,magic...
 
   (2) for the open-ended/ yes-no questions, do we make the "object" the actual "question"..
 
-  ( ) change QUESTIONS....  
+  ( ) change QUESTIONS....
 
 '''
