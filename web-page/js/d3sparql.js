67b6fbb8d35a0af8f30fd8c4a0913c77fcc2144c
Dustin-Lee <dustinlchang@gmail.com>
2015-08-10 13:48:35 -0700

    ForceGraph stays after drag

--- web-page/js/d3sparql.js
+++ web-page/js/d3sparql.js
@@ -1005,10 +1005,12 @@ d3sparql.forcegraph = function(json, config) {
   }
 
   if(json != null) {
+    console.log('not the NULL');
     var svg = d3.select(opts.selector).html("").append("svg")
       .attr("width", opts.width)
       .attr("height", opts.height)
   } else {
+    console.log('in the NULL');
     //var svg = d3.select(opts.selector).html('<div class="nodata_outer"><div class="nodata_mid"><div class="nodata_inner"><h1>No Data Recieved With Current Query Search</h1></div></div></div>').append("svg")
     var svg = d3.select(opts.selector).html('<div class="nodata_center"><h1>No Data Recieved With Current Query Search</h1></div>').append("svg")
       .attr("width", opts.width)
@@ -1021,19 +1023,56 @@ d3sparql.forcegraph = function(json, config) {
   .domain(d3.extent(graph.nodes, function(d) {return parseFloat(d.value)}))
   .range([1, 200])
 
-  console.log(graph);
-  console.log(graph.nodes.length);
-
   var link = svg.selectAll(".link")
   .data(graph.links)
   .enter()
   .append("line")
   .attr("class", "link")
+  //Testing here
+  .attr("x1", function(d) {return d.source.x})
+  .attr("y1", function(d) {return d.source.y})
+  .attr("x2", function(d) {return d.target.x})
+  .attr("y2", function(d) {return d.target.y})
+
+  var node_drag = d3.behavior.drag()
+    .on("dragstart", dragstart)
+    .on("drag", dragmove)
+    .on("dragend", dragend);
+
+  function dragstart(d, i) {
+    console.log(d+'|||'+i);
+    force.stop() // stops the force auto positioning before you start dragging
+  }
+
+  function dragmove(d, i) {
+    console.log(d+'|||'+i);
+    d.px += d3.event.dx;
+    d.py += d3.event.dy;
+    d.x += d3.event.dx;
+    d.y += d3.event.dy;
+    tick(); // this is the key to make it work together with updating both px,py,x,y on d !
+  }
+
+  function dragend(d, i) {
+    console.log(d+'|||'+i);
+    d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+    tick();
+    force.resume();
+  }
+
   var node = svg.selectAll(".node")
   .data(graph.nodes)
   .enter()
   .append("g")
   .style("fill", function(d) { return d.group; })
+  .call(node_drag);
+
+
+  console.log(link);
+  console.log(node);
+  console.log(opts.distance);
+  console.log(opts.charge);
+
   var circle = node.append("circle")
   .attr("class", "node")
   .attr("r", opts.radius)
@@ -1043,8 +1082,8 @@ d3sparql.forcegraph = function(json, config) {
   var force = d3.layout.force()
   .charge(opts.charge)
   .linkDistance(opts.distance)
-  .gravity(0.01)
-  .linkStrength(0.001)
+  .gravity(0.05)
+  //.linkStrength(0.001)
   .size([opts.width, opts.height])
   .nodes(graph.nodes)
   .links(graph.links)
@@ -1059,6 +1098,32 @@ d3sparql.forcegraph = function(json, config) {
     .attr("cy", function(d) { return d.y = Math.max(opts.radius, Math.min(opts.height - opts.radius, d.y)); });
   })*/
   .start()
+  console.log(force);
+/*
+  setTimeout(function() {
+    console.log('in the tim');
+    console.log(force);
+    //force.stop();
+    force.gravity(0.01);
+    force.linkStrength(0.001);
+    console.log(force);
+  }, 3000);
+*/
+
+
+  force.on("tick", tick);
+
+  function tick() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  };
+
+
+
   /*setTimeout(function() {
     console.log('in the tim');
     force.gravity(0);
@@ -1066,7 +1131,7 @@ d3sparql.forcegraph = function(json, config) {
   }, 5000);*/
   //for (var i = 0; i < 100; i++) {
     //force.tick();
-    force.on("tick", function() {
+/*    force.on("tick", function() {
       console.log('in the tick!!!!!!!!!!!!!!!!!!');
       link.attr("x1", function(d) {return d.source.x})
       .attr("y1", function(d) {return d.source.y})
@@ -1077,13 +1142,14 @@ d3sparql.forcegraph = function(json, config) {
       circle.attr("cx", function(d) { return d.x = Math.max(opts.radius, Math.min(opts.width - opts.radius, d.x)); })
       .attr("cy", function(d) { return d.y = Math.max(opts.radius, Math.min(opts.height - opts.radius, d.y)); });
     })
+*/
     //force.linkStrength(0)
     console.log('after tick');
   //}//End of for loop
   console.log('after FOR tick');
-  setTimeout(function() {
-    force.stop();
-  }, 5000);
+  //setTimeout(function() {
+  //  force.stop();
+  //}, 5000);
 
 
   /*setTimeout(function() {
@@ -1094,7 +1160,7 @@ d3sparql.forcegraph = function(json, config) {
     force.linkStrength(1);
     console.log(force);
   }, 3000);*/
-  node.call(force.drag)
+  ///////////////node.call(force.drag)
   //force.stop();
   // default CSS/SVG
   link.attr({
