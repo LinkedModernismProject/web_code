6c2447d989e15bb718b250b4e947a53e7372429d
dustinchang <dustinlchang@gmail.com>
2015-12-02 15:28:08 -0800

    got all values into format for form

--- convert/turtle_form.py
+++ convert/turtle_form.py
@@ -11,42 +11,38 @@ for line in f:
     if re.match('entity_name:\s+(.+)', line) is not None:
         ent_match = re.match('entity_name:\s+(.+)', line)
         ent = ent_match.group(1)
-        print ent
-    elif re.match('(alt_entity_names|birth_date|death_date|selSexuality|selGender|selSex|ethnic|plang|createdBy|place):\s+(.+)', line) is not None:
-        names = re.match('(alt_entity_names|birth_date|death_date|selSexuality|selGender|selSex|ethnic|plang|createdBy|place|occupation_|lang_|hasInflu_|wasInflu_|relatedPlace_).*:\s+(.+)', line)
+    elif re.match('(alt_entity_names|birth_date|death_date|occupation_|selSexuality|selGender|selSex|ethnic|pLang|lang_|hasInflu_|wasInflu_|createdBy|place|relatedPlace_).*:\s+(.+)', line) is not None:
+        names = re.match('(alt_entity_names|birth_date|death_date|occupation_|selSexuality|selGender|selSex|ethnic|pLang|lang_|hasInflu_|wasInflu_|createdBy|place|relatedPlace_).*:\s+(.+)', line)
         if names.group(1) == 'alt_entity_names':
             fo.write('{}\t\t{}\t\thasAlias\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'birth_date'):
+        elif names.group(1) == 'birth_date':
             fo.write('{}\t\t{}\t\thasBirthdate\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'death_date'):
+        elif names.group(1) == 'death_date':
             fo.write('{}\t\t{}\t\tterminatesIn\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'selSexuality'):
+        elif names.group(1) == 'occupation_':
+            fo.write('{}\t\t{}\t\ttypeOfPerson\n'.format(ent, names.group(2)))
+        elif names.group(1) == 'selSexuality':
             fo.write('{}\t\t{}\t\thasSexuality\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'selGender'):
+        elif names.group(1) == 'selGender':
             fo.write('{}\t\t{}\t\thasGender\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'selSex'):
+        elif names.group(1) == 'selSex':
             fo.write('{}\t\t{}\t\thasSex\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'ethnic'):
+        elif names.group(1) == 'ethnic':
             fo.write('{}\t\t{}\t\tassociatedWithEthnicity\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'plang'):
+        elif names.group(1) == 'pLang':
             fo.write('{}\t\t{}\t\tusesPrimaryLanguage\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'createdBy'):
+        elif names.group(1) == 'lang_':
+            fo.write('{}\t\t{}\t\tusesLanguage\n'.format(ent, names.group(2)))
+        elif names.group(1) == 'hasInflu_':
+            fo.write('{}\t\t{}\t\tinfluenced\n'.format(ent, names.group(2)))
+        elif names.group(1) == 'wasInflu_':
+            fo.write('{}\t\t{}\t\tinfluencedBy\n'.format(ent, names.group(2)))
+        elif names.group(1) == 'createdBy':
             fo.write('{}\t\t{}\t\tcreatedBy\n'.format(ent, names.group(2)))
-        elif names.group(1 == 'place'):
+        elif names.group(1) == 'place':
             fo.write('{}\t\t{}\t\toriginatingCountry\n'.format(ent, names.group(2)))
-        elif:
-
-
-
-#hasAlias
-
-    elif None:
-        pass
-
-
-
-
-
+        elif names.group(1) == 'relatedPlace_':
+            fo.write('{}\t\t{}\t\tassociatedWithCountry\n'.format(ent, names.group(2)))
 
 f.close()
 fo.close()
