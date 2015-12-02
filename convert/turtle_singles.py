d238c292f80b96917b80d374128cf86f896f5fe2
dustinchang <dustinlchang@gmail.com>
2015-12-01 16:10:27 -0800

    changed to put in Sub Obj Pred format

--- convert/turtle_singles.py
+++ convert/turtle_singles.py
@@ -1,16 +1,16 @@
 import re
 import sys
 #Pass an input file arg and an output file arg, originally wrote to take out_triples.txt
+#Ex to run: python turtle_singles.py test_ts.txt out_turt_sing.txt
 open(sys.argv[2], 'a').close()	#To create output file if hasn't been already
 f = open(sys.argv[1], 'r')
 fo = open(sys.argv[2], 'r+')
 
-fo.write('@prefix pref0: <http://modernism.uvic.ca/metadata#> . \n@prefix pref1: <http://localhost:8890/limo#> . \n@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . \n\n')
+pred_holder = ''
 
 for line in f:
     if re.match('Subject:\s+(.+)', line) is not None:
-        subj_match = re.match('Subject:\s+(.+)', line)
-        fo.write('<http://modernism.uvic.ca/metadata#'+ subj_match.group(1).replace(' ', '_') + '>\n')
+        fo.write(re.match('Subject:\s+(.+)', line).group(1) + '\t\t')
     elif re.match('(\s+)(Predicate|Object):\s+(.+)', line) is not None:
         po_match = re.match('(\s+)(Predicate|Object):\s+(.+)', line)
         po_list = po_match.group(3).split(' ')
@@ -21,12 +21,9 @@ for line in f:
                     po_camel += s.capitalize()
 
         if po_match.group(2) == 'Predicate':
-            fo.write(po_match.group(1) + 'pref1:' + po_camel + ' "')
+            pred_holder = po_camel + '\n'
         elif po_match.group(2) == 'Object':
-            fo.write(' '.join(po_list) + '" .\n\n')
-
-    elif re.match('(\s+)Object:\s+(.+)', line) is not None:
-        pass
+            fo.write(' '.join(po_list) + '\t\t' + pred_holder)
     else:
         pass #Shouldn't get here in any case, except the end of the file
 
