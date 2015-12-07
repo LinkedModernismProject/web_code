2ccf1708055846a46966b5794df77e1d560a2d56
dustinchang <dustinlchang@gmail.com>
2015-12-07 14:18:54 -0800

    can read from file directly into arr

--- automation/batch_add_data.sh
+++ automation/batch_add_data.sh
@@ -2,8 +2,12 @@
 user="vspdemo"
 file="$1"
 file2="$2"
-echo "Hello, $file, file2"
+echo "Hello, $file, $file2"
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
+while IFS=$'\t' read -r -a arr; do
     echo "Text read from file: $line"
+    echo "${arr[0]}"
+    echo "${arr[1]}"
+    echo "${arr[2]}"
+    #echo $line | cut -d $'\t' -f2
 done < "$1"
