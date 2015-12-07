cd0b17915d97dfa4c3b334e5306c93d97e4723b5
dustinchang <dustinlchang@gmail.com>
2015-12-07 10:35:59 -0800

    descriptions and cleanup

--- convert/db_updater.sh
+++ convert/db_updater.sh
@@ -8,26 +8,26 @@ if [[ "insert" != "$ins_del" && "delete" != "$ins_del" ]] ; then
 	exit 0
 fi
 echo "Please enter the following information (Case Sensitive):"
-echo "Please enter the Subject you want deleted :"
-read -r del_subj
-echo "Please enter the Predicate you want deleted :"
-read -r del_pred
-echo "Please enter the Object you want deleted :"
-read -r del_obj
+echo "Please enter the Subject you want inserted/deleted (use _ instead of spaces to separate words):"
+read -r ins_del_subj
+echo "Please enter the Predicate you want inserted/deleted (use camel case ex: typeOfPerson):"
+read -r ins_del_pred
+echo "Please enter the Object you want inserted/deleted:"
+read -r ins_del_obj
 echo "Please enter your SPARQL Password: "
 read -sr SPARQL_PASSWORD
 
 if [ "insert" = "$ins_del" ] ; then
 	echo "inserting~~~"
 	./isql 1111 $user $SPARQL_PASSWORD <<-EOF
-	  sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:$del_subj  pref1:$del_pred  "$del_obj" } ;
+	  sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:$ins_del_subj  pref1:$ins_del_pred  "$ins_del_obj" } ;
 	  EXIT;
 	EOF
 	echo "Thank you for entering data."
 elif [ "delete" = "$ins_del" ] ; then
 	echo "deleting~~~"
 	./isql 1111 $user $SPARQL_PASSWORD <<-EOF
-	  sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> DELETE DATA FROM <http://localhost:8890/dustin> { meta:$del_subj  pref1:$del_pred  "$del_obj" } ;
+	  sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> DELETE DATA FROM <http://localhost:8890/dustin> { meta:$ins_del_subj  pref1:$ins_del_pred  "$ins_del_obj" } ;
 	  EXIT;
 	EOF
 	echo "Thank you for cleaning up our data."
