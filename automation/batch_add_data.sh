6abd6d4c3d0558595224ee57d9c37a034fbe5b8a
dustinchang <dustinlchang@gmail.com>
2015-12-07 16:27:48 -0800

    cleanup & slight description

--- automation/batch_add_data.sh
+++ automation/batch_add_data.sh
@@ -2,17 +2,14 @@
 user="vspdemo"
 file="$1"
 file2="$2"
-echo "Hello, $file, $file2"
 echo "Please enter your SPARQL Password: "
 read -sr SPARQL_PASSWORD
 
 for file in *.txt; do
-  	echo "!!!$file!!!"
+	echo -e "\n--- Processing $file ---"
   	##Read from file and delimit on tabs (any number of them) and read through array
   	while IFS=$'\t' read -r -a arr; do
-  		echo "hwile"
 	   if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
-	   		echo "in the if Z"
 		     ./isql 1111 $user $SPARQL_PASSWORD <<-EOF
 		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
 				EXIT;
