0a209f089d08dd12862ddcd72375039c72562397
dustinchang <dustinlchang@gmail.com>
2015-12-08 10:29:20 -0800

    cleanup & notes on running

--- automation/batch_add_data.sh
+++ automation/batch_add_data.sh
@@ -5,16 +5,18 @@ file2="$2"
 echo "Please enter your SPARQL Password: "
 read -sr SPARQL_PASSWORD
 
+#Goes through all .txt files in current dir and adds all triples within them
+#txt files need to end in an empty line
 for file in *.txt; do
-	echo -e "\n--- Processing $file ---"
+	echo -e "\n|||---Processing $file---|||"
   	##Read from file and delimit on tabs (any number of them) and read through array
   	while IFS=$'\t' read -r -a arr; do
-	   if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
+	  	if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
 		     ./isql 1111 $user $SPARQL_PASSWORD <<-EOF
 		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
 				EXIT;
 			EOF
-        fi
+      fi
 	done < "$file"
 done
 echo -e "\nThank you for entering data."
