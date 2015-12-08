bccb110130f115408e97d1e53993fe41007ba73f
dustinchang <dustinlchang@gmail.com>
2015-12-07 16:17:06 -0800

    can read all txt files with current dir

--- automation/batch_add_data.sh
+++ automation/batch_add_data.sh
@@ -6,13 +6,18 @@ echo "Hello, $file, $file2"
 echo "Please enter your SPARQL Password: "
 read -sr SPARQL_PASSWORD
 
-#Read from file and delimit on tabs (any number of them) and read through array
-while IFS=$'\t' read -r -a arr; do
-	if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
-		./isql 1111 $user $SPARQL_PASSWORD <<-EOF
-		  sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
-		  EXIT;
-		EOF
-	fi
-done < "$1"
+for file in *.txt; do
+  	echo "!!!$file!!!"
+  	##Read from file and delimit on tabs (any number of them) and read through array
+  	while IFS=$'\t' read -r -a arr; do
+  		echo "hwile"
+	   if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
+	   		echo "in the if Z"
+		     ./isql 1111 $user $SPARQL_PASSWORD <<-EOF
+		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
+				EXIT;
+			EOF
+        fi
+	done < "$file"
+done
 echo -e "\nThank you for entering data."
