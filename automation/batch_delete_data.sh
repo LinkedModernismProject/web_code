8edd9ab1cc939fdded81ccbcab0aa8cc6c7203b5
dustinchang <dustinlchang@gmail.com>
2015-12-09 12:24:11 -0800

    backup copy of bestDataProduction, scripts modified for use in weeklyRun dir

--- automation/batch_delete_data.sh
+++ automation/batch_delete_data.sh
@@ -10,7 +10,7 @@ for file in *.txt; do
   	##Read from file and delimit on tabs (any number of them) and read through array
   	while IFS=$'\t' read -r -a arr; do
 	  	if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
-		     ./isql 1111 $user $SPARQL_PASSWORD <<-EOF
+		     ../../../../../../../bin/isql 1111 $user $SPARQL_PASSWORD <<-EOF
 		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> DELETE DATA FROM <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
 				EXIT;
 			EOF
