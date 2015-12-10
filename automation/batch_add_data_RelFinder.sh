1d49ff9205e92522242d96c45c89ec07fb70398d
dustinchang <dustinlchang@gmail.com>
2015-12-10 15:57:46 -0800

    batch addition to RelFinder works

--- automation/batch_add_data_RelFinder.sh
+++ automation/batch_add_data_RelFinder.sh
@@ -10,13 +10,12 @@ for file in *.txt; do
   	##Read from file and delimit on tabs (any number of them) and read through array
   	while IFS=$'\t' read -r -a arr; do
 	  	if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
+	  		one_word_subj="${arr[0]// /_}"
 	  		one_word_obj="${arr[1]// /_}"
 	  		str_subj="${arr[0]//_/ }"
-	  		echo $one_word_obj
-	  		echo $str_subj
 		     ../../../../../../../bin/isql 1111 $user $SPARQL_PASSWORD <<-EOF
-		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata/> PREFIX pref1: <http://localhost:8890/limo/> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  pref1:${arr[2]}  meta:$one_word_obj } ;
-		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata/> PREFIX pref1: <http://localhost:8890/limo/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> INSERT DATA INTO <http://localhost:8890/dustin> { meta:${arr[0]}  rdfs:${arr[2]} "$str_subj" } ;
+		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata/> PREFIX pref1: <http://localhost:8890/limo/> INSERT DATA INTO <http://localhost:8890/bestDataRelFinder> { meta:$one_word_subj  pref1:${arr[2]}  meta:$one_word_obj } ;
+		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata/> PREFIX pref1: <http://localhost:8890/limo/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> INSERT DATA INTO <http://localhost:8890/bestDataRelFinder> { meta:$one_word_subj  rdfs:label "$str_subj" } ;
 				EXIT;
 			EOF
       fi
