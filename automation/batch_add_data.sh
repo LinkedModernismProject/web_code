5efc4d5adb65470cf3ffea93aa1cfcce348a424c
dustinchang <dustinlchang@gmail.com>
2015-12-10 16:28:53 -0800

    make to add to both search and relfinder

--- automation/batch_add_data.sh
+++ automation/batch_add_data.sh
@@ -10,8 +10,9 @@ for file in *.txt; do
   	##Read from file and delimit on tabs (any number of them) and read through array
   	while IFS=$'\t' read -r -a arr; do
 	  	if [ ! -z "${arr[0]}" ] && [ ! -z "${arr[1]}" ] && [ ! -z "${arr[2]}" ] ; then #To stop from grabbing blank lines in arg file given
+	  		one_word_subj="${arr[0]// /_}"
 		     ../../../../../../../bin/isql 1111 $user $SPARQL_PASSWORD <<-EOF
-		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/bestDataProduction> { meta:${arr[0]}  pref1:${arr[2]}  "${arr[1]}" } ;
+		       sparql PREFIX meta: <http://modernism.uvic.ca/metadata#> PREFIX pref1: <http://localhost:8890/limo#> INSERT DATA INTO <http://localhost:8890/bestDataProduction> { meta:$one_word_subj  pref1:${arr[2]}  "${arr[1]}" } ;
 				EXIT;
 			EOF
       fi
